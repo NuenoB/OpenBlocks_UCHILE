65dca51612e07fdd3ad9cf4eac42e68392df8545
Agustin Antoine <agustin@niclabs.cl>
2014-04-10 22:41:00 -0400

    Changes in ModInfo values, Name and version updated

--- src/openblocks/ModInfo.java
+++ src/openblocks/ModInfo.java
@@ -2,8 +2,8 @@ package openblocks;
 
 public class ModInfo {
 	public static final String ID = "OpenBlocks";
-	public static final String NAME = "OpenBlocks";
-	public static final String VERSION = "@VERSION@";
+	public static final String NAME = "OpenBlocks_UCHILE";
+	public static final String VERSION = "0 .0";
 	public static final String PROXY_SERVER = "openblocks.common.ServerProxy";
 	public static final String PROXY_CLIENT = "openblocks.client.ClientProxy";
 	public static final String DEPENDENCIES = "required-after:OpenMods@[0.4,];after:OpenPeripheral;after:NotEnoughCodecs";
