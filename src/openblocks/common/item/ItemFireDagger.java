f515b941b56dddd5775e9b27709648872d4d9639
Camila Alvarez <camosita@yahoo.es>
2014-04-24 12:33:24 -0300

    Clase general para armas cortantes

--- src/openblocks/common/item/ItemFireDagger.java
+++ src/openblocks/common/item/ItemFireDagger.java
@@ -12,30 +12,14 @@ import net.minecraft.item.ItemStack;
 public class ItemFireDagger extends ItemDagger {
 	
 	public ItemFireDagger(int idItem){
-		super(idItem);
-	}
-	
-	/** Registrar la imagen del item **/
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void registerIcons(IconRegister registry) {
-		itemIcon = registry.registerIcon("openblocks:fire_dagger");
+		super(idItem,"fire_dagger");
 	}
 	
 	@Override
 	/** Efecto cuando se golpea un enemigo **/
 	public boolean hitEntity(ItemStack item, EntityLivingBase enemy, EntityLivingBase player){
-		item.damageItem(1, player);
 		enemy.setFire(10);
-		return true;
-	}
-	
-	/** Agrega información del objeto **/
-	@Override
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
-		super.addInformation(itemStack, player, list, par4);
-		list.add("Burns the enemy");
+		return super.hitEntity(item, enemy, player);
 	}
 
 }
