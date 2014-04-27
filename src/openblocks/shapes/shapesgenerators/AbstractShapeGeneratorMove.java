ecf0621253d8d6210ab5d951f544b40a0a2ba893
roma <roma@amermelao>
2014-04-27 18:39:52 -0400

    Se crea para que los dhapegeneratormove almacenen sus coordenadas

---
+++ src/openblocks/shapes/shapesgenerators/AbstractShapeGeneratorMove.java
@@ -0,0 +1,33 @@
+package openblocks.shapes.shapesgenerators;
+
+import java.util.ArrayList;
+
+import net.minecraft.block.Block;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.World;
+import openblocks.shapes.BlockRepresentation;
+import openmods.shapes.IShapeable;
+
+public abstract class AbstractShapeGeneratorMove implements IShapeGenenratorMove {
+
+
+
+	protected int moriginX = 0;
+	protected int moriginY = 0;
+	protected int moriginZ = 0;
+
+	@Override
+	public void setConstrictionPosition(int x, int y, int z) {
+		moriginX = x;
+		moriginY = y;
+		moriginZ = z;
+		
+	}
+	
+	@Override
+	public ArrayList<BlockRepresentation> fill(ChunkCoordinates entityPos,
+			World worldObj) {
+		
+		return new ArrayList<BlockRepresentation>();
+	}
+}
