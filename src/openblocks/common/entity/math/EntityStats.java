1087da325a4c443e3c518d1e6d7bcdc184ff2e49
JoshepLeonhart <joseign.m@gmail.com>
2014-05-29 17:52:49 -0400

    Agregados finalmente método que ataca a dos entidades usando los stats del personaje. José Ignacio Moreno

--- src/openblocks/common/entity/math/EntityStats.java
+++ src/openblocks/common/entity/math/EntityStats.java
@@ -1,5 +1,7 @@
 package openblocks.common.entity.math;
 
+import java.util.Random;
+
 import net.minecraft.entity.EntityLivingBase;
 
 public abstract class EntityStats {
@@ -14,6 +16,7 @@ public abstract class EntityStats {
 	public abstract float getMAG();
 	public abstract float getRES();
 	public abstract int getSPD();
+	public abstract DamageType getDMGType();
 	
 	public float getHP() {
 		return hitPoints;
@@ -23,9 +26,33 @@ public abstract class EntityStats {
 		return magicPoints;
 	}
 	
-	public abstract void attackTo(EntityStats enemy);
+	public final double attackTo(EntityStats enemy) {
+		return enemy.beingDamaged(this.getDMGType(), this.getATK(), this.getSPD());
+	}
 	
-	public abstract void beingDamaged(DamageType type, float baseDMG);
+	public final double beingDamaged(DamageType type, float baseDMG, int enemySPD) {
+		// calculo de evasion
+		double avo = (enemySPD-this.getSPD())/100.0;
+		if (avo > (new Random()).nextGaussian()) {
+			return -1;
+		}
+		// uso de DEF/RES segun tipo de ataque
+		if (type == DamageType.PHYSICAL) {
+			baseDMG -= this.getDEF();
+		}
+		else {
+			baseDMG -= this.getRES();
+		}
+		// calculo daño final
+		baseDMG = baseDMG * (85 + (new Random()).nextInt(16));
+		baseDMG = Math.min(baseDMG, this.hitPoints);
+		// causar daño
+		this.hitPoints -= baseDMG;
+		if (this.hitPoints <= 0) {
+			this.getEntity().setDead();
+		}
+		return baseDMG;
+	}
 	
 	public abstract EntityLivingBase getEntity();
 	
