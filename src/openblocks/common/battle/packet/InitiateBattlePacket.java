2b9951164aae9fe36172dbf0c62ccc64dbe55e0b
RittoShadow <rittoshadow@gmail.com>
2014-06-17 12:58:01 -0400

    Creado el paquete openblocks.common.battle.packet para enviar paquetes al server para proveer la comunicación entre cliente y servidor, como se sugirió en un issue.

---
+++ src/openblocks/common/battle/packet/InitiateBattlePacket.java
@@ -0,0 +1,28 @@
+package openblocks.common.battle.packet;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+
+public class InitiateBattlePacket extends BattlePacket {
+
+	@Override
+	public void write(ByteArrayDataOutput out) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void read(ByteArrayDataInput in) throws ProtocolException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void execute(EntityPlayer player) throws ProtocolException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
